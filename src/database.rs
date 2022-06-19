2fa06f2f82c1b76eeaf00adad8062926800963c8
ldevillez <louis.devillez@gmail.com>
2022-06-19 15:48:21 +0200

    select db from settings

--- src/database.rs
+++ src/database.rs
@@ -3,6 +3,7 @@ use std::error::Error;
 use lazy_static::lazy_static;
 use tokio::sync::Mutex;
 use rusqlite::{Connection, Result, params};
+use std::cell::RefCell;
 
 type AsyncResult<T> = Result<T, Box<dyn Error + Send + Sync>>;
 
@@ -10,13 +11,14 @@ use crate::bth;
 use crate::user_config;
 use crate::mytime;
 
+
 lazy_static! {
-    static ref CONNECTION: Mutex<Connection> = Mutex::new(Connection::open("./birthday.db3").unwrap());
+    static ref CONNECTION: Mutex<RefCell<Connection>> = Mutex::new(RefCell::new(Connection::open("./birthday.db3").unwrap()));
 }
 
 pub async fn initialize_database() -> AsyncResult<()> {
 
-  CONNECTION.lock().await.execute(
+  CONNECTION.lock().await.borrow().execute(
         "CREATE TABLE IF NOT EXISTS person (
             id       INTEGER PRIMARY KEY,
             user_id  INTEGER,
@@ -27,7 +29,7 @@ pub async fn initialize_database() -> AsyncResult<()> {
         )",
         [], // empty list of parameters.
     )?;
-  CONNECTION.lock().await.execute(
+  CONNECTION.lock().await.borrow().execute(
         "CREATE TABLE IF NOT EXISTS config (
             id       INTEGER PRIMARY KEY,
             user_id  INTEGER,
@@ -41,9 +43,7 @@ pub async fn initialize_database() -> AsyncResult<()> {
 
 pub async fn get_all_birthdays(user_id: i32) -> AsyncResult<Vec<bth::Birthday>> {
     let mut birthdays: Vec<bth::Birthday> = Vec::new();
-    let c = CONNECTION.lock().await;
-    let mut statement = c.prepare(&format!("SELECT name, day, month, reminder, id FROM person WHERE user_id ={}", user_id))?;
-    let birthdays_map = statement.query_map([], |row| {
+    for birthday in CONNECTION.lock().await.borrow().prepare(&format!("SELECT name, day, month, reminder, id FROM person WHERE user_id ={}", user_id))?.query_map([], |row| {
         Ok(bth::Birthday {
             name: row.get(0)?,
             day: row.get(1)?,
@@ -51,36 +51,32 @@ pub async fn get_all_birthdays(user_id: i32) -> AsyncResult<Vec<bth::Birthday>> 
             reminder: row.get(3)?,
             id: row.get(4)?,
         })
-    })?;
-
-    for birthday in birthdays_map {
+    })? {
         birthdays.push(birthday.unwrap())
     }
     Ok(birthdays)
 }
 
 pub async fn create_birthday(user_id: i32, birthday: &bth::Birthday) -> AsyncResult<()> {
-    CONNECTION.lock().await.execute("INSERT INTO person (user_id, name, day, month, reminder) VALUES (?1, ?2, ?3, ?4, ?5)", params![user_id, birthday.name, birthday.day, birthday.month, birthday.reminder],)?;
+    CONNECTION.lock().await.borrow().execute("INSERT INTO person (user_id, name, day, month, reminder) VALUES (?1, ?2, ?3, ?4, ?5)", params![user_id, birthday.name, birthday.day, birthday.month, birthday.reminder],)?;
 
     Ok(())
 }
 
 pub async fn delete_birthday(user_id: i32, id: i32) -> AsyncResult<()>{
-    CONNECTION.lock().await.execute("DELETE FROM person WHERE id=?1 AND user_id=?2", params![id,user_id],)?;
+    CONNECTION.lock().await.borrow().execute("DELETE FROM person WHERE id=?1 AND user_id=?2", params![id,user_id],)?;
 
     Ok(())
 }
 
 pub async fn edit_birthday(user_id: i32, birthday: &bth::Birthday) -> AsyncResult<()>{
-    CONNECTION.lock().await.execute("UPDATE person SET name = ?1, day = ?2, month = ?3, reminder=?4 WHERE id = ?5 AND user_id = ?6", params![birthday.name, birthday.day, birthday.month, birthday.reminder, birthday.id,user_id],)?;
+    CONNECTION.lock().await.borrow().execute("UPDATE person SET name = ?1, day = ?2, month = ?3, reminder=?4 WHERE id = ?5 AND user_id = ?6", params![birthday.name, birthday.day, birthday.month, birthday.reminder, birthday.id,user_id],)?;
 
     Ok(())
 }
 
 pub async fn get_config(user_id: i32) -> AsyncResult<user_config::UserConfig>{
-    let c = CONNECTION.lock().await;
-    let mut statement = c.prepare(&format!("SELECT hour, minute FROM config WHERE user_id ={}", user_id))?;
-    Ok(statement.query_row([],|row| {
+    Ok(CONNECTION.lock().await.borrow().prepare(&format!("SELECT hour, minute FROM config WHERE user_id ={}", user_id))?.query_row([],|row| {
         //println!("Loadead, {:?}",row.get(0));
         Ok(user_config::UserConfig {
             hour: row.get(0)?,
@@ -90,22 +86,20 @@ pub async fn get_config(user_id: i32) -> AsyncResult<user_config::UserConfig>{
 }
 
 pub async fn edit_config(user_id: i32, config: &user_config::UserConfig) -> AsyncResult<()>{
-    CONNECTION.lock().await.execute("UPDATE config SET hour = ?1, minute = ?2 WHERE user_id = ?3", params![config.hour, config.minute, user_id],)?;
+    CONNECTION.lock().await.borrow().execute("UPDATE config SET hour = ?1, minute = ?2 WHERE user_id = ?3", params![config.hour, config.minute, user_id],)?;
 
     Ok(())
 }
 
 pub async fn create_config(user_id: i32, config: &user_config::UserConfig) -> AsyncResult<()>{
-    CONNECTION.lock().await.execute("INSERT INTO config (hour, minute, user_id) VALUES (?1, ?2, ?3)", params![config.hour, config.minute, user_id],)?;
+    CONNECTION.lock().await.borrow().execute("INSERT INTO config (hour, minute, user_id) VALUES (?1, ?2, ?3)", params![config.hour, config.minute, user_id],)?;
 
     Ok(())
 }
 
 pub async fn get_all_config(prev: &mytime::time, curr: &mytime::time) -> AsyncResult<Vec<i32>> {
     let mut uconf_ids: Vec<i32> = Vec::new();
-    let c = CONNECTION.lock().await;
-    let mut statement = c.prepare("SELECT hour, minute, user_id FROM config")?;
-    let conf_map = statement.query_map([], |row| {
+    for id in CONNECTION.lock().await.borrow().prepare("SELECT hour, minute, user_id FROM config")?.query_map([], |row| {
         let conf_time = mytime::time {
             hour: row.get(0)?,
             minute: row.get(1)?
@@ -116,8 +110,7 @@ pub async fn get_all_config(prev: &mytime::time, curr: &mytime::time) -> AsyncRe
             Ok(-1)
         }
 
-    })?;
-    for id in conf_map {
+    })? {
         let i = id.unwrap();
         if i != -1 {
             uconf_ids.push(i);
@@ -125,4 +118,8 @@ pub async fn get_all_config(prev: &mytime::time, curr: &mytime::time) -> AsyncRe
     }
     Ok(uconf_ids)
 
+}
+
+pub async fn set_db(path: String) -> () {
+    CONNECTION.lock().await.replace(Connection::open(path).unwrap());
 }
--- src/main.rs
+++ src/main.rs
@@ -20,8 +20,8 @@ type AsyncResult<T> = Result<T, Box<dyn Error + Send + Sync>>;
 #[derive(serde::Deserialize)]
 pub struct Conf {
     bot_token: String,
-    user_id: i64,
-    db_name: String,
+    user_id: i64, // For admin purpose
+    db_name: String, //path to db
 }
 
 
@@ -211,37 +211,19 @@ async fn answer(
 
 #[tokio::main]
 async fn main() {
-
     let path = "birthday_tg_rs";
     let mut settings = config::Config::default();
     settings.merge(config::File::with_name(path)).unwrap();
+
+    println!("Loading conf");
+    let conf: Conf = settings.try_into().unwrap();
+
+    database::set_db(conf.db_name).await;
 
     println!("Initializing database");
     let status_db = database::initialize_database();
     status_db.await.expect("Could not initialize database");
 
-
-    println!("Loading conf");
-    let conf: Conf = settings.try_into().unwrap();
-    //println!("Token is {}", conf.bot_token);
-    //println!("User ID is {}",conf.user_id);
-
-    let mut bth = bth::Birthday {
-        name: "Muncho".to_owned(),
-        day: 21,
-        month: 8,
-        reminder: 6,
-        id: 3
-    };
-    let status_db = database::create_birthday(357669106, &bth);
-    status_db.await.expect("Could not add birthday");
-    //bth.reminder = 4;
-    //let status_db = database::edit_birthday(10, &bth);
-    //status_db.await.expect("Could edit birthday");
-    //let status_db = database::delete_birthday(10, 2);
-    //status_db.await.expect("Could not delete birthday");
-    //let result = database::get_all_birthdays(10).await.unwrap();
-    //println!("Result: {:?}", result);
 
     println!("Starting bot...");
     let bot = Bot::new(&conf.bot_token).auto_send();
@@ -252,7 +234,6 @@ async fn main() {
         interval.tick().await;
 
         loop {
-            println!("Hello !");
             interval.tick().await;
             let current = mytime::get_mytime_from_time();
             let ids:Vec<i32> = database::get_all_config(&previous,&current).await.unwrap();
@@ -274,7 +255,6 @@ async fn main() {
 
     });
 
-    //bot.send_message(357669106, "Hey").await;
     teloxide::repls2::commands_repl(bot, answer, Command::ty()).await;
 
 // TODO logging
